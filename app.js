ab329b758b35ebb095e8c89081efdb0c62709eb5
Feliciter <padakluev@gmx.com>
2019-11-03 23:26:20 +0200

    some change sett

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
 import './src/scss/common.scss'
-//import {handler} from './src/js/javascript'
-
- import {} from './src/js/javascript'
-
- import searchFunction from './src/js/javascript'
-// import {searchFunction} from './src/js/javascript'
-
-// handler()
+import {} from './src/js/javascript'
